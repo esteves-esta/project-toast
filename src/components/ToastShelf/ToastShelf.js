d4a2d5d3be9f5434eaa2aa54c2c6af5ed06f3e57
Fernanda Esteves <fernanda.esteves@clear.sale>
2023-11-22 11:36:02 -0300

    feat: ex 5.2

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -8,7 +8,7 @@ function ToastShelf() {
   const { removeToast, toastList } = React.useContext(ToastContext)
 
   return (
-    <ol className={styles.wrapper}>
+    <ol className={styles.wrapper} role="region" aria-live="polite" aria-label="Notification">
       {toastList.map(({ id, message, variant }) => (
         <li key={id} className={styles.toastWrapper}>
           <Toast onClose={() => removeToast(id)} variant={variant}>{message}</Toast>
